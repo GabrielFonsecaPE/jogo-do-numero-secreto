76090407ff85a5a3f12add171d798564c22cfccc
GabrielFonsecaPE <gabrielvictorfonseca@gmail.com>
2024-08-22 17:28:23 -0300

    valor máximo é 100

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um número entre 1 à 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 mensagemInicial();
@@ -18,7 +18,7 @@ function exibirTextoNaTela (tag, texto){
 
 function mensagemInicial(){
 exibirTextoNaTela ('h1', 'Jogo do número secreto');
-exibirTextoNaTela ('p', 'Digite um número de 1 a 10');
+exibirTextoNaTela ('p', 'Digite um número de 1 a 100');
 }
 
 //função sem parametros e sem retorno. executa um comando.
